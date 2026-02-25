f3893058d69fc30fa2a5e2e9beebaa82473b1ca4
P-cyber162 <anakwaprince204@gmail.com>
2026-02-25 22:11:16 +0000

    first commit

--- index.java
+++ index.java
@@ -2,23 +2,12 @@ import java.util.Scanner;
 
 public class index {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int radius=2;
-        double area, circumferece
-        
-        System.out.println("========AREA OF A CIRCLE=========");
-        System.out.println("==========================");
-        System.out.println(); 
-        System.out.println("Enter the radius of the circle: ");    
-        radius = input.nextInt();
-        area = Math.PI*radius*radius;
-        circumferece = 2*Math.PI*radius;
+       for(int i=0; i <= 5; i++){
+        for(int j=1; j<=i; j++){
+            System.out.print("* ");
+        }
 
-        System.out.println("====CIRCUMFERENCE OF THE CIRLCE=====");
-        System.out.println("====================================");
-        System.out.println("Area of the circle is: " + area);
-        System.out.println("Circumference of the circle is: " + circumferece);
-
-        input.close();
+        System.out.println();
+       }
     }
 };
