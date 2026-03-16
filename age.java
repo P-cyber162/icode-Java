4b47432d58a3317348acde62438a1eff9c104cf7
P-cyber162 <anakwaprince204@gmail.com>
2026-03-16 22:08:55 +0000

    first commit

---
+++ age.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class age {
+    public static void main(String args[]) {
+        String firstName = "Prince";
+        String lastName = "Anakwa";
+        int age = 20;
+        int height = 32;
+        boolean isStudent = age <= 21;
+
+        System.out.println("----------Personal  Info Card-------------");
+        System.out.println("First Name :" + firstName);
+        System.out.println("Last Name :" + lastName);
+        System.out.println("Age :" + age);
+        System.out.println("Height :" + height);
+        System.out.println("Student :" + isStudent);
+        System.out.println("-------------------------------------------");
+    }
+}
