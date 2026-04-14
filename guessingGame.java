0fed67133ace7ec9b376129177ad8b958a5b8b98
P-cyber162 <anakwaprince204@gmail.com>
2026-04-14 22:47:14 +0000

    update readme

---
+++ guessingGame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; // Taking user input
+import java.util.Random;
+
+public class guessingGame {
+    public static void main(String[]args) {
+        int[] arr = new int[10];
+        int value = 2;
+
+        for (int i=0; i< arr.length; i++) {
+            arr[i]= value;
+            value +=2;
+        }
+        int sum = 0;
+        for(int i=0; i< arr.length; i++) {
+            sum += arr[i];
+        }
+
+        int average = sum/arr.length;
+
+        int factorial = 1;
+        for(int i=0; i<= average; i++) {
+            factorial *= i;
+        };
+    }
+}
